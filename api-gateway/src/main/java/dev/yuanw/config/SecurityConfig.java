1c2757f5f0843dfb93721b2a06e970362135231d
Yuan Wang <yuanwang@dal.ca>
2023-08-26 14:31:56 -0300

    feat: implement OAuth2.0 on API Gateway using Keycloak

---
+++ api-gateway/src/main/java/dev/yuanw/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package dev.yuanw.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
+
+        return serverHttpSecurity.csrf()
+                .disable()
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/eureka/**").permitAll()   // permit eureka static resources
+                        .anyExchange().authenticated())    // authenticate all others
+                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt).build();
+    }
+}
